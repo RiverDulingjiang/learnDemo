458034647099462203b21e12f0b59629eda425e8
river <425567941@qq.com>
2018-09-02 09:32:46 +0800

    测试base_role access contral

--- learn/src/main/java/com/river/learn/web/action/MytestAction.java
+++ learn/src/main/java/com/river/learn/web/action/MytestAction.java
@@ -2,6 +2,7 @@ package com.river.learn.web.action;
 
 import java.util.List;
 
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +39,12 @@ public class MytestAction {
 		 DBIdentifier.setProjectCode(projectCode);
 		 List<DataSourceBean> bean = mapper.get();		
 		 return  new Gson().toJson(bean);
+	 }
+	 
+	 @RequestMapping("/rbac.htm")
+	 @ResponseBody
+	 @RequiresPermissions("")
+	 public String rbac() {
+	       return "Hello World!";
 	 }
 }
