2e2ae11e51dfd824f0822170e9dde9a3217d6530
duRiver <425567941@qq.com>
2018-09-17 14:19:14 +0800

    删除导入的无用依赖

--- learn/src/main/java/com/river/main/App.java
+++ learn/src/main/java/com/river/main/App.java
@@ -1,14 +1,9 @@
 package com.river.main;
 
-import java.nio.charset.Charset;
-
 import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @ComponentScan(value = {"com.river", "com.river.learn" })
